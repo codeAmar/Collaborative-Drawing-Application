7d5960c357181bfeb4db87cca64769cd3f35caad
Amarjot singh <amarjotsingh90@yahoo.com>
2018-02-18 00:12:53 -0800

    brush is working

--- public/javascripts/draw1.js
+++ public/javascripts/draw1.js
@@ -1,15 +1,27 @@
 var path, pointX, pointY;
 var initialPointRect, initialPointTri, initialPointCir, initialPointPen, initialPointBrush, intialSizeTri = 50;
-var rectangle, triangle, circle, pen;
+var rectngle, triangle, circle, pen;
 var rectangleDrawn = false;
 var isPenDrawing = false;
 var isBrushDrawing = false;
 var cpyarr;
 var initialPointPenCopy;
+var path_to_send = {};
+
+var brushPathObj = {};
 
 var colors = document.querySelectorAll('div.color-pallet');
 var tools = document.querySelectorAll('div.tool-pallet');
 
+tool.minDistance = 10;
+tool.maxDistance = 45;
+
+var uid = (function () {
+    var S4 = function () {
+        return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+    };
+    return (S4() + S4() + "-" + S4() + "-" + S4() + "-" + S4() + "-" + S4() + S4() + S4());
+}());
 
 
 var current = {
@@ -28,26 +40,23 @@ colors.forEach(function (element) {
 });
 
 tools.forEach(function (element) {
-    // element.style.backgroundColor = element.className.split(' ')[0];
     element.addEventListener('click', function (event) {
         if (event.type === 'click') {
             current.tool = event.target.className.split(' ')[0];
             console.log(current.tool);
         }
-    })
+    });
 });
 
 
 
 function randomColor() {
-
     return {
         red: 0,
         green: Math.random(),
         blue: Math.random(),
         alpha: (Math.random() * 0.25) + 0.05
     };
-
 }
 
 function onMouseDown(event) {
@@ -75,11 +84,19 @@ function onMouseDown(event) {
         case 'pen':
             initialPointPen = new Path();
             initialPointPen.strokeColor = current.color;
+            initialPointPen.add(event.point);
             isPenDrawing = true;
+            path_to_send = {
+                start: {
+                    "x": event.point.x,
+                    "y": event.point.y
+                },
+                path: []
+            };
             break;
         case 'brush':
             initialPointBrush = new Path();
-            mouseDownDrawBrush(initialPointBrush);
+            mouseDownDrawBrush(initialPointBrush, event);
             break;
         default:
             console.log('unknown switch statement');
@@ -89,12 +106,23 @@ function onMouseDown(event) {
 
 function onMouseDrag(event) {
     if (isPenDrawing) {
-        initialPointPen.add(event.point);
+        mouseDragDrawPen(event);
     }
     if (isBrushDrawing) {
         mouseDragDrawBrush(event);
     }
 }
+
+function mouseDragDrawPen(event) {
+    console.log('event.point mousedrag ', event.point.x, event.point.y);
+    initialPointPen.add(event.point);
+    path_to_send.path.push({
+        x: event.point.x,
+        y: event.point.y
+    });
+}
+
+
 
 
 
@@ -125,6 +153,7 @@ function onMouseUp(event) {
             triangle = new Path.RegularPolygon(initialPointTri, 3, intialSizeTri);
             triangle.fillColor = current.color;
             triangle.selected = true;
+            emitTri(initialPointTri, 3, intialSizeTri);
             break;
         case 'circle':
             circle = new Path.Circle(initialPointCir, 60);
@@ -133,23 +162,16 @@ function onMouseUp(event) {
             emitCir(initialPointCir, 60);
             break;
         case 'pen':
-            isPenDrawing = false;
-            initialPointPenCopy = Object.assign({},initialPointPen);
-            console.log('mouse released from pen' + Object.keys(initialPointPenCopy) );
-            console.log('initial pen data ' + Object.keys(initialPointPen));
-            emitPen(initialPointPenCopy);
+            mouseUpDrawPen(event);
             break;
         case 'brush':
             isBrushDrawing = false;
             MouseUpDrawBrush(event);
-            // var copy = initialPointBrush.clone();
-            // copy.position = new Point(100, 100);
             break;
         default:
             console.log('unknown switch statement');
             break;
     }
-
     if (rectangleDrawn) {
         rectangle.onMouseEnter = function (event) {
             this.fillColor = 'cyan';
@@ -166,38 +188,68 @@ function onMouseUp(event) {
 
 
 
-function mouseDownDrawBrush(initialPointBrush) {
+function mouseUpDrawPen(event) {
+    isPenDrawing = false;
+    initialPointPen.add(event.point);
+    initialPointPen.closed = false;
+    initialPointPen.smooth();
+    console.log('whole pen path ', initialPointPen);
+    path_to_send.end = {
+        "x": event.point.x,
+        "y": event.point.y
+    };
+    io.emit('drawPens', JSON.stringify(path_to_send));
+    path_to_send.path = new Array();
+}
+
+
+
+function mouseDownDrawBrush(initialPointBrush, event) {
     initialPointBrush.fillColor = current.color;
     initialPointBrush.add(event.point);
     console.log('brush path mouse pressed positions' + initialPointBrush.position);
     isBrushDrawing = true;
+    brushPathObj = {
+        start: {
+            "x": event.point.x,
+            "y": event.point.y
+        },
+        events: [],
+        end: {}
+    };
 }
 
 function mouseDragDrawBrush(event) {
     console.log('brush path drag positions' + initialPointBrush.position);
-
     var step = event.delta / 2;
     step.angle += 90;
-
     var top = event.middlePoint + step;
     var bottom = event.middlePoint - step;
+    console.log('top', top);
+    console.log('bottom', bottom);
 
     initialPointBrush.add(top);
     initialPointBrush.insert(0, bottom);
     initialPointBrush.smooth();
+    brushPathObj.events.push({
+        "top": top,
+        "bottom": bottom,
+    });
+
 }
 
 
 function MouseUpDrawBrush(event) {
+    isBrushDrawing = false;
     initialPointBrush.add(event.point);
     initialPointBrush.closed = true;
     initialPointBrush.smooth();
-    console.log('brush path mouse up positions' + initialPointBrush);
-    console.log(initialPointBrush._segments[0]._point._x);
-    cpyarr = Object.assign({}, initialPointBrush);
-    console.log(cpyarr);
-    emitBrush(cpyarr);
-
+    brushPathObj.end = {
+        "x": event.point.x,
+        "y": event.point.y
+    };
+    io.emit('drawBrushs', JSON.stringify(brushPathObj));
+    brushPathObj.events = new Array();
 }
 
 
@@ -230,13 +282,22 @@ function emitCir(points, size) {
     io.emit('drawCir', data);
 }
 
+function emitTri(points, corners, size) {
+    var data = {
+        points: points,
+        corners: corners,
+        size: size
+    }
+    io.emit('drawTri', data);
+}
+
 function emitBrush(path) {
     io.emit('drawBrush', path);
 }
 
-function emitPen(path){
-    console.log('emit pen '+ path);
-    io.emit('drawPen',path);
+function emitPen(path) {
+    console.log('emit pen ' + path);
+    io.emit('drawPen', path);
 }
 
 
@@ -263,20 +324,48 @@ io.on('drawCir', function (data) {
     myCircle.fillColor = current.color;
 })
 
-io.on('drawBrush', function (path) {
+io.on('drawTri', function (data) {
+    console.log('data recieved : ', data);
+    var emittedTriangle = new Path.RegularPolygon(new Point(data.points[1], data.points[2]), data.corners, data.size);
+    emittedTriangle.fillColor = current.color;
+    emittedTriangle.selected = true;
+    console.log('emitted triangle  : ', emittedTriangle);
     view.draw();
-
-    var brushClone = new Path(path._segments);
-    brushClone.fillColor = current.color;
-    console.log(brushClone)
-    view.draw();
-    // brushClone.fillColor= 'red';
-    // console.log('draw brush event recieved',brushClone);   
 })
 
-io.on('drawPen', function(path){
-    console.log(path);
-    // var myPenClone = path.clone();
-    // console.log('mypen clone '+ myPenClone);
+
+io.on('drawBrushs', function (paths) {
+    view.draw();
+    var brushSegments = JSON.parse(paths);
+    console.log(brushSegments);
+    var BrushPaths = new Path();
+    BrushPaths.fillColor = current.color;
+    BrushPaths.add(new Point(brushSegments.start.x, brushSegments.start.y));
+    var brushDragPaths = brushSegments.events.forEach(function (element) {
+        var segment_arr_top = element.top.slice(1);
+        var segment_arr_bottom = element.bottom.slice(1);
+        BrushPaths.add(new Point(segment_arr_top[0], segment_arr_top[1]));
+        BrushPaths.insert(0, new Point(segment_arr_bottom[0], segment_arr_bottom[1]));
+        BrushPaths.smooth();
+    });
+    BrushPaths.add(new Point(brushSegments.end.x, brushSegments.end.y));
+    BrushPaths.closed = true;
+    BrushPaths.smooth();
+    console.log('drawbrushs ', path);
+})
+
+
+io.on('drawPens', function (paths) {
+    var paths = JSON.parse(paths);
+    var emitPen = new Path();
+    emitPen.add(new Point(paths.start.x, paths.start.y));
+    emitPen.strokeColor = current.color;
+    console.log('paths', paths);
+    var myresuolt = paths.path.forEach(function (item) {
+        emitPen.add(new Point(item.x, item.y));
+    });
+    console.log(myresuolt);
+    emitPen.add(new Point(paths.end.x, paths.end.y));
+    console.log('emitPen ', emitPen);
     view.draw();
 })
